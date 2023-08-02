8df4eb75bf1d61422d15fa9eda7847686fb6b6e3
DiNo-4 <120940819+Dino-4@users.noreply.github.com>
2023-08-03 03:51:14 +0400

    origin

--- src/BonusMilesService.java
+++
@@ -1,8 +0,0 @@
-public class BonusMilesService {
-   public int calculate(int ticketPrice){
-       int priceOfOneMile = 20;
-       int miles = ticketPrice / priceOfOneMile;
-
-       return miles;
-   }
-}
